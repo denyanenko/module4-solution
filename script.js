f4d426c8dafba084ca6ff815d6abefc194c5f96e
denyanenko <108097508+denyanenko@users.noreply.github.com>
2022-12-30 04:51:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ for (var i = 0; i < names.length; i++) {
 
   if (firstLetter === 'j') {
     byeSpeaker.speak(names[i]);
-  } else {
-    helloSpeaker.speak(names[i]);
-  }
+       } 
+       else {
+    helloSpeaker.speak(names[i]);}
 }})();
